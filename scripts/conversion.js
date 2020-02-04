7f62d26eb43a50729708373ec0cb571cce5b9de7
jp367 <jeanphilippes42069@gmail.com>
2020-02-04 07:13:49 +0100

    Added hex2payload function and created temporary template to use it in conversion.html (edited style accordingly)

--- scripts/conversion.js
+++ scripts/conversion.js
@@ -28,15 +28,46 @@ function convert(number, base, options) {
   return result;
 }
 
-console.log(convert(12, 2, {
-  padding: 12,
-  size: 4,
-  separator: '_'
-}))
+/**
+ * Convert hex number to a valid payload of the form \xDE\xAD\xBE\xEF
+ * 
+ * @param {Number} number - Hexadecimal number
+ * @param {Boolean} le - Indicates if using little-endian format
+ * 
+ * @returns {string} - String like \xDE\xAD\xBE\xEF from 0xdeadbeef
+ */
+function hex2payload(number, le = true) {
+  if (number.startsWith("0x")) number = number.slice(2);
+  number = chunkString(number, 2, true);
+  if (number[0].length % 2) number[0] = '0' + number[0];
+  if (le) number = number.reverse();
+  number = number.map(x => "\\x" + x);
+  return number.join('');
+}
+
+// console.log(convert(12, 2, {
+//   padding: 12,
+//   size: 4,
+//   separator: '_'
+// }))
+
+// console.log(hex2payload('0x8007625a'))
 
 const conversionButton = document.getElementById('conversion-button');
 conversionButton.addEventListener('click', e => {
   const textAreas = document.getElementsByTagName('textarea');
   const values = [...textAreas].map(t => t.value);
-  console.log(values);
+  // console.log(values);
+});
+
+const tempButton = document.getElementById('temp-button');
+tempButton.addEventListener('click', e => {
+  const inp = document.getElementById("in");
+  const out = document.getElementById("out");
+  const check = document.getElementById("endian-checkbox");
+  try {
+    out.value = hex2payload(inp.value.trim(), check.checked);
+  } catch (error) {
+    console.log(error);
+  }
 });
